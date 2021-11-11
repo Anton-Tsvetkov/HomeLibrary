0dd2deb65eb4b270658214d15d615d5ad24b15fb
Anton Tsvetkov <anton_tsvetkov@epam.com>
2021-11-11 22:02:24 +0300

    filter by stream implemented

--- src/main/java/com/epam/laboratory/util/BookFinder.java
+++ src/main/java/com/epam/laboratory/util/BookFinder.java
@@ -3,41 +3,34 @@ package com.epam.laboratory.util;
 import com.epam.laboratory.entities.library.Author;
 import com.epam.laboratory.entities.library.Book;
 import com.epam.laboratory.entities.library.Bookmark;
+import com.epam.laboratory.entities.system.Librarian;
 import com.epam.laboratory.entities.user.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookFinder {
 
     public static List<Book> getBooksByTitleAndPagesAndYear(List<Book> bookList, String title, int pagesAmount, int issueYear) {
-        List<Book> foundBooks = new ArrayList<>();
-        for (Book book : bookList) {
-            if (book.getTitle().contains(title)
-                    || (book.getPagesAmount() == pagesAmount)
-                    || (book.getIssueYear() == issueYear)) {
-                foundBooks.add(book);
-            }
-        }
-        return foundBooks;
+        return bookList
+                .stream()
+                .filter(book -> book.getTitle().contains(title)
+                        && book.getPagesAmount() == pagesAmount
+                        && book.getIssueYear() == issueYear)
+                .collect(Collectors.toList());
     }
 
     public static List<Book> getBooksByTitle(List<Book> bookList, String title) {
-        List<Book> foundBooks = new ArrayList<>();
-        for (Book book : bookList) {
-            if (book.getTitle().contains(title)) {
-                foundBooks.add(book);
-            }
-        }
-        return foundBooks;
+        return bookList.stream().filter(book -> book.getTitle().contains(title)).collect(Collectors.toList());
     }
 
-    public static List<Book> getBooksByAuthor(List<Book> bookList, String author) {
+    // still thinking
+    public static List<Book> getBooksByAuthor(List<Book> bookList, String authorName) {
         List<Book> foundBooks = new ArrayList<>();
         for (Book book : bookList) {
-            List<Author> authorsList = book.getAuthorList();
-            for (Author author1 : authorsList) {
-                if (author1.getName().contains(author)) {
+            for (Author bookAuthor : book.getAuthorList()) {
+                if (bookAuthor.getName().contains(authorName)) {
                     foundBooks.add(book);
                 }
             }
@@ -46,32 +39,19 @@ public class BookFinder {
     }
 
     public static List<Book> getBooksByISBN(List<Book> bookList, String isbn) {
-        List<Book> foundBooks = new ArrayList<>();
-        for (Book book : bookList) {
-            if (book.getISBN().equals(isbn)) {
-                foundBooks.add(book);
-            }
-        }
-        return foundBooks;
+        return bookList.stream().filter(book -> book.getISBN().contains(isbn)).collect(Collectors.toList());
     }
 
     public static List<Book> getBooksByIssueYear(List<Book> bookList, int issueYearFrom, int issueYearTo) {
-        List<Book> foundBooks = new ArrayList<>();
-        for (Book book : bookList) {
-            if (book.getIssueYear() >= issueYearFrom
-                    && book.getIssueYear() <= issueYearTo) {
-                foundBooks.add(book);
-            }
-        }
-        return foundBooks;
+        return bookList.stream().filter(book -> book.getIssueYear() >= issueYearFrom && book.getIssueYear() <= issueYearTo).collect(Collectors.toList());
     }
 
-    public static List<Book> getBooksWithUsersBookmarks(List<Book> bookList, User user){
+    // still thinking
+    public static List<Book> getBooksWithUsersBookmarks(List<Book> bookList, User user) {
         List<Book> bookListWithUsersBookmarks = new ArrayList<>();
-        List<Bookmark> usersBookmark = user.getBookmarkList();
         for (Book book : bookList) {
-            for (Bookmark bookmark : usersBookmark) {
-                if (book.getISBN().equals(bookmark.getISBN())){
+            for (Bookmark bookmark : user.getBookmarkList()) {
+                if (book.getISBN().equals(bookmark.getISBN())) {
                     bookListWithUsersBookmarks.add(book);
                 }
             }
